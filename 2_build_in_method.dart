b0d062257fc54cccc8a4a2bb675995079e1be10b
Varun pc <Varunrevathi6@gmail.com>
2024-08-22 10:04:59 +0530

    String interPoltation And Const & final keywords

--- 2_build_in_method.dart
+++ 2_build_in_method.dart
@@ -1,26 +1,26 @@
-
 void main(List<String> arguments) {
+  // Numbers: int
+  int score = 23;
+  var count = 23; // It is inferred as integer automatically by compiler
+  int hexValue = 0xEADEBAEE;
+  print(count);
 
-	// Numbers: int
-	int score = 23;
-	var count = 23;     // It is inferred as integer automatically by compiler
-	int hexValue = 0xEADEBAEE;
+  // Numbers: double
+  double percentage = 93.4;
+  var percent = 82.533;
+  double exponents = 1.42e5;
+  print("Numbers like Double: $percentage $percent $exponents");
 
-	// Numbers: double
-	double percentage = 93.4;
-	var percent = 82.533;
-	double exponents = 1.42e5; 
+  // Strings
+  String name = "Henry";
+  var company = "Google";
+  print("Strings are : $name $company");
 
-	// Strings
-	String name = "Henry";
-	var company = "Google";
+  // Boolean
+  bool isValid = true;
+  var isAlive = false;
+  print("Conditional statements condition $isValid or condition $isAlive");
 
-	// Boolean
-	bool isValid = true;
-	var isAlive = false;
-
-	print(score);
-	print(hexValue);
-
-	
+  print(score);
+  print(hexValue);
 }
